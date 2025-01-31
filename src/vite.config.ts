d023f36fc39a3ec5438f93ef8fafdd8b3a594792
Raphael Schmitz <raphaelschmitz00@gmail.com>
2025-01-31 21:14:03 +0100

    Some cleanup

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,11 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
-const env = loadEnv('', process.cwd(), '')
-console.log('BBB', env.VITE_PUBLIC_URL)
 
 export default defineConfig({
   plugins: [
