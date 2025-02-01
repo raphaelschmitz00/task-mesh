a59c9dd6ef39358df67bef7e75c867962d56fcb3
Raphael Schmitz <raphaelschmitz00@gmail.com>
2025-02-01 23:41:29 +0100

    Remove remaining assets and views

--- src/src/router/index.ts
+++ src/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import OverViewPage from '@/pages/tasks/OverViewPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,25 +7,12 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: OverViewPage,
     },
     {
       path: '/status',
       name: 'status',
       component: () => import('../pages/status/StatusBoardPage.vue'),
-    },
-    {
-      path: '/tasks',
-      name: 'tasks',
-      component: () => import('../pages/tasks/OverViewPage.vue'),
     },
   ],
 })
--- src/src/widgets/PageHeader.vue
+++ src/src/widgets/PageHeader.vue
@@ -5,8 +5,6 @@
     <nav>
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/status">Status Board</RouterLink>
-      <RouterLink to="/tasks">Tasks</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
     </nav>
   </header>
 </template>
