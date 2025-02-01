5b60f4f637d79570bd0a7617897120ae95f5cc14
Raphael Schmitz <raphaelschmitz00@gmail.com>
2025-02-01 23:32:46 +0100

    Clean up PageHeader

--- src/src/widgets/PageHeader.vue
+++ src/src/widgets/PageHeader.vue
@@ -1,86 +1,12 @@
-<script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    <div class="greetings">
-      <h1 class="green">{{ msg }}</h1>
-      <h3>
-        You’ve successfully created a project with
-        <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-        <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
-      </h3>
-    </div>
-
+  <header>
     <nav>
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/status">Status Board</RouterLink>
       <RouterLink to="/tasks">Tasks</RouterLink>
       <RouterLink to="/about">About</RouterLink>
     </nav>
-  </div>
+  </header>
 </template>
-
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
