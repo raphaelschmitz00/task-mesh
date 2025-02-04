83387fffeb586d4635deb606c8dd656e3be3690a
Raphael Schmitz <raphaelschmitz00@gmail.com>
2025-02-04 21:14:24 +0100

    Add DependencyEditorDialog

---
+++ src/src/pages/taskEditor/DependencyEditorDialog.vue
@@ -0,0 +1,103 @@
+<script setup lang="ts">
+import { computed, reactive, watch } from 'vue'
+import { type Task, useTaskStore } from '@/stores/Task'
+
+const model = defineModel<boolean>()
+
+const props = defineProps<{
+  task: Task
+}>()
+
+class State {
+  addDependencyDialogIsShown = false
+  task?: Task
+  availableTasks: Task[] = []
+  chosenTasks: Task[] = []
+}
+
+const state = reactive(new State())
+
+const taskStore = useTaskStore()
+
+const unchosenTasks = computed(() =>
+  state.availableTasks.filter((x) => !state.chosenTasks.includes(x)),
+)
+
+function fetchTask() {
+  state.availableTasks = taskStore.allTasks.filter((x) => x != props.task)
+  state.chosenTasks = state.availableTasks.filter((x) => props.task.dependsOn.includes(x.key))
+}
+
+function addDependency(task: Task) {
+  state.chosenTasks.push(task)
+}
+function removeDependency(task: Task) {
+  const index = state.chosenTasks.indexOf(task)
+  state.chosenTasks.splice(index, 1)
+}
+
+function exitSavingChanges() {
+  const chosenIds = state.chosenTasks.map((x) => x.key)
+  taskStore.update({ ...props.task, dependsOn: chosenIds })
+  model.value = false
+}
+
+function exitDiscardingChanges() {
+  model.value = false
+}
+
+watch(() => props.task, fetchTask)
+
+fetchTask()
+</script>
+
+<template>
+  <q-dialog v-model="model">
+    <q-card>
+      <q-card-section>
+        <h2>Add Dependency</h2>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <span>Required Tasks</span>
+        <q-list bordered>
+          <q-item
+            v-for="requiredTask in state.chosenTasks"
+            :key="requiredTask.key"
+            clickable
+            v-ripple
+            @click="removeDependency(requiredTask)"
+          >
+            <q-item-section>{{ requiredTask.name }}</q-item-section>
+            <q-item-section avatar>
+              <q-icon color="primary" name="remove_circle" />
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <span>Available</span>
+        <q-list bordered>
+          <q-item
+            v-for="requiredTask in unchosenTasks"
+            :key="requiredTask.key"
+            clickable
+            v-ripple
+            @click="addDependency(requiredTask)"
+          >
+            <q-item-section>{{ requiredTask.name }}</q-item-section>
+            <q-item-section avatar>
+              <q-icon color="primary" name="add_circle" />
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-card-section>
+
+      <q-card-actions align="right">
+        <q-btn flat label="Cancel" color="primary" v-close-popup @click="exitDiscardingChanges" />
+        <q-btn flat label="Save Changes" color="primary" v-close-popup @click="exitSavingChanges" />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
--- src/src/pages/taskEditor/TaskEditorPage.vue
+++ src/src/pages/taskEditor/TaskEditorPage.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import { computed, reactive } from 'vue'
+import { reactive } from 'vue'
 import { useRouter } from 'vue-router'
-import { type Task, TaskStatus, useTaskStore } from '@/stores/Task'
+import { type Task, useTaskStore } from '@/stores/Task'
 import StandardCard from '@/styling/StandardCard.vue'
 import TaskStatusDropdown from '@/widgets/TaskStatusDropdown.vue'
-import UpdateTaskWidget from '@/widgets/UpdateTaskWidget.vue'
 import TaskStatusView from '@/widgets/TaskStatusView.vue'
 import StandardButton from '@/styling/StandardButton.vue'
+import DependencyEditorDialog from './DependencyEditorDialog.vue'
 
 const props = defineProps<{
   idString: string
@@ -22,15 +22,9 @@ const state = reactive(new State())
 const taskStore = useTaskStore()
 const router = useRouter()
 
-const availableTasks = computed(() =>
-  taskStore.allTasks
-    .filter((x) => x.key != state.task?.key)
-    .filter((x) => !state.task?.dependsOn.includes(x.key)),
-)
-
-const chosenTasks: { [id: number]: boolean } = {}
-
-function addChosenTasks() {}
+function openDependencyDialog() {
+  state.addDependencyDialogIsShown = true
+}
 
 function fetchTask() {
   const id = parseInt(props.idString)
@@ -47,14 +41,6 @@ function deleteTask() {
   router.back()
 }
 
-function addDependency() {
-  const availableTasks = taskStore.allTasks
-    .filter((x) => x.key != state.task?.key)
-    .filter((x) => !state.task?.dependsOn.includes(x.key))
-
-  state.task!.dependsOn.push(5)
-}
-
 fetchTask()
 </script>
 
@@ -68,36 +54,8 @@ fetchTask()
         <div v-for="id in state.task.dependsOn" :key="id">Task # {{ id }}</div>
       </div>
 
-      <StandardButton icon="add" label="Add" @click="state.addDependencyDialogIsShown = true" />
-      <q-dialog v-model="state.addDependencyDialogIsShown">
-        <q-card>
-          <q-card-section>
-            <div class="text-h6">Add Dependency</div>
-          </q-card-section>
-
-          <q-card-section class="q-pt-none">
-            <div v-for="id in state.task.dependsOn" :key="id">Task # {{ id }}</div>
-          </q-card-section>
-
-          <q-card-section class="q-pt-none">
-            <q-list>
-              <q-item v-for="task in availableTasks" :key="task.key">
-                <q-item-section side top>
-                  <q-checkbox v-model="chosenTasks[task.key]" />
-                </q-item-section>
-              </q-item>
-
-              <div v-for="task in availableTasks" :key="task.key">
-                {{ task.key }} - {{ task.name }}
-              </div>
-            </q-list>
-          </q-card-section>
-
-          <q-card-actions align="right">
-            <q-btn flat label="OK" color="primary" v-close-popup />
-          </q-card-actions>
-        </q-card>
-      </q-dialog>
+      <StandardButton icon="add" label="Add" @click="openDependencyDialog" />
+      <DependencyEditorDialog v-model="state.addDependencyDialogIsShown" :task="state.task" />
     </div>
     <div>
       <TaskStatusView :status="state.task.status" class="statusRowItem" />
