07bc0f83c0609f5625e553727661ee520e044f4d
Raphael Schmitz <49372721+raphaelschmitz00@users.noreply.github.com>
2025-02-10 22:46:58 +0100

    Make Task Editor a Dialog (#31)
    * Dialog working
    
    * Remove unused files
    
    * Clean up dialog structure

---
+++ src/src/widgets/editTaskDialog/DialogContent.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { type Task, useTaskStore } from "@/stores/Task";
+import StandardButton from "@/components/buttons/TmButton.vue";
+import TmCard from "@/components/cards/TmCard.vue";
+import TmCardSection from "@/components/cards/TmCardSection.vue";
+import DeadlineSection from "./DeadlineSection.vue";
+import RequirementSection from "./RequirementSection.vue";
+import StatusSection from "./StatusSection.vue";
+
+const props = defineProps<{
+  task: Task;
+}>();
+
+const emit = defineEmits<{
+  doneEditing: [];
+}>();
+
+const taskStore = useTaskStore();
+function updateTask() {
+  taskStore.save(props.task);
+  emit("doneEditing");
+}
+
+function deleteTask() {
+  taskStore.remove(props.task);
+  emit("doneEditing");
+}
+</script>
+
+<template>
+  <TmCard v-if="task">
+    <TmCardSection>
+      <h1>{{ `#${task.id} - ${task.name}` }}</h1>
+    </TmCardSection>
+
+    <TmCardSection>
+      <DeadlineSection :task="task" />
+    </TmCardSection>
+
+    <TmCardSection>
+      <RequirementSection :task="task" />
+    </TmCardSection>
+
+    <TmCardSection>
+      <StatusSection :task="task" />
+    </TmCardSection>
+
+    <TmCardSection>
+      <StandardButton icon="delete" label="Delete" @click="deleteTask" />
+      <StandardButton icon="save" label="Save" @click="updateTask" />
+    </TmCardSection>
+  </TmCard>
+</template>
