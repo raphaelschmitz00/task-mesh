c5b7dc7f3c505577ba533390383b8097bd178849
Raphael Schmitz <raphaelschmitz00@gmail.com>
2025-02-18 23:51:49 +0100

    Fix eslint

--- src/eslint.config.ts
+++ src/eslint.config.ts
@@ -12,7 +12,13 @@ export default defineConfigWithVueTs(
   {
     name: "app/files-to-lint",
     files: ["**/*.{ts,mts,tsx,vue}"],
+  },
+  {
+    name: "app/files-to-ignore",
     ignores: ["**/dist/**", "**/dist-ssr/**", "**/coverage/**"],
+  },
+  {
+    name: "app/single-word-component-names",
     rules: {
       "vue/multi-word-component-names": "off",
     },
